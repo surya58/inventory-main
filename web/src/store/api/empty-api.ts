6c188d41fcc87ea9159074ffa14984f160385e9d
Contractor1 <contractor1@Sidleys-MacBook-Pro.local>
2025-08-14 11:19:46 -0500

    updated basequery

--- web/src/store/api/empty-api.ts
+++ web/src/store/api/empty-api.ts
@@ -2,7 +2,10 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const emptySplitApi = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_PYTHON_API_URL }),
+  //baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_PYTHON_API_URL }),
+  baseQuery: fetchBaseQuery({ 
+    baseUrl: process.env.NEXT_PUBLIC_PYTHON_API_URL || 'http://127.0.0.1:8001'
+  }),
   endpoints: () => ({}),
 })
 
